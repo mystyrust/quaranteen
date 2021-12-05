6d26500a6612a37036764f38eb8dfcf9b9057013
mystyrust <mystyrust@gmail.com>
2021-12-05 12:39:58 -0600

    CH7 file explorer wip arrow clicker , password clicker

--- Quaranteens/CH7/FileExplorer/fileExplorer.js
+++ Quaranteens/CH7/FileExplorer/fileExplorer.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
     $(".sidebar-item").click((event) => {
-        // var classes = $(target.currentTarget).attr("class");
-        // console.log(classes)
         var target = event.currentTarget.innerText.toLowerCase()
         console.log(target)
-
     });
+
+    $(".arrow-reverse").click(() => {
+        window.history.back();
+    }) 
+
+    $(".arrow").click(() => {
+        window.history.forward();
+    }) 
+
+    $("#submit").click((event)=> {
+        var pwd = $("#pwd").val();
+
+        if (pwd == "asdf") // show view
+        {
+            $(".incorrect-pwd").css('visibility', 'hidden');
+            $(".locked-view").css('display', 'none');
+            $(".unlocked-view").css('display', 'block');
+        } else {
+            $(".incorrect-pwd").css('visibility', 'visible');
+        }
+    })
 })
