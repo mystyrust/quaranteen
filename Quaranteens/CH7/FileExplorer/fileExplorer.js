6dfff3526606f0f032fb80d6ca5ae4f6fca8dd36
mystyrust <mystyrust@gmail.com>
2021-12-07 14:06:27 -0600

    CH7 blackmail + weapons dir, 2 download btns, init 'history'

--- Quaranteens/CH7/FileExplorer/fileExplorer.js
+++ Quaranteens/CH7/FileExplorer/fileExplorer.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
     // console.log('ready')
     $(".hint").css('display', 'none')
     var currentTarget = location.hash
-    // var fileBrowserHistory = []
+
+    var fileBrowserHistory = []
+    var historyIndex = 0
 
     if (currentTarget)
     {
@@ -22,7 +24,8 @@ $(document).ready(function() {
         $(currentHint).css('display', 'table')
 
         // location.replace("#" + target)
-        // fileBrowserHistory[historyIndex] = target;
+        fileBrowserHistory[historyIndex] = target;
+        historyIndex++
     });
 
     $(".arrow-reverse").click(() => {
@@ -36,21 +39,25 @@ $(document).ready(function() {
     $("#submit").click((event) => {
         var pwd = $("#pwd").val();
 
-        if (pwd == "asdf") // show view
+        if (pwd == "iLov3Maddie") // show view
         {
             $(".incorrect-pwd").css('visibility', 'hidden');
             $(".locked-view").css('display', 'none');
             $(".unlocked-view").css('display', 'block');
 
-            $(".unlocked-hint").css('display', 'table');
-            $(".locked-hint").css('display', 'none');
+            $(".locked-hint").remove();
+
+            $(".documents-hint-unlocked")
+                .removeClass("documents-hint-unlocked")
+                .addClass("documents-hint")
+                .css('display', 'table');
             
         } else {
             $(".incorrect-pwd").css('visibility', 'visible');
         }
     })
 
-    $(".download-all").click(() => {
+    $(".download-final").click(() => {
         console.log('clicked download')
         // downloading
         $(".download-all").toggle()
@@ -67,7 +74,42 @@ $(document).ready(function() {
             $(".research .document-list").toggle()
             $(".loader").toggle()
             $(".loader-complete").toggle()
+
             // then show new danny hint afterwards too
+            // hide all previous hints
+            $(".research-downloaded").removeClass("hint").css('display', 'table');
+            $(".hint:not(.research-downloaded)").remove();
+        }, 2000)
+    })
+
+    $(".download-weapons").click(() => {
+        console.log('clicked download')
+        // downloading
+        $(".download-all").toggle()
+        $(".weapons .document-list").toggle()
+        $(".loader").toggle()
+
+        // then, download complete
+        setTimeout(() => { 
+            $(".loader-complete").toggle()
+        }, 1000)
+
+        setTimeout(() => { 
+            $(".download-all").toggle()
+            $(".weapons .document-list").toggle()
+            $(".loader").toggle()
+            $(".loader-complete").toggle()
+
+            // then show new danny hint afterwards too
+
+            $(".weapons-hint").remove();
+            $(".weapons-hint-downloaded ")
+                .removeClass("weapons-hint-downloaded ")
+                .addClass("weapons-hint")
+                .css('display', 'table');
+            // hide all previous hints
+            // $(".research-downloaded").removeClass("hint").css('display', 'table');
+            // $(".hint:not(.research-downloaded)").remove();
         }, 2000)
     })
 
